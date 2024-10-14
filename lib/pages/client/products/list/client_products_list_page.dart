17ea83a4e7130c63781c9069fc07d0c06709e176
Xeppyz <alexander.xampos10@gmail.com>
2024-10-13 23:48:37 -0600

    feat(drawer):done

--- lib/pages/client/products/list/client_products_list_page.dart
+++ lib/pages/client/products/list/client_products_list_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:shoes/pages/client/products/list/client_products_list_controller.dart';
+import 'package:shoes/src/utils/my_colors.dart';
 
 class ClienteProductsListPage extends StatefulWidget {
   const ClienteProductsListPage({super.key});
@@ -18,12 +19,13 @@ class _ClienteProductsListPageState extends State<ClienteProductsListPage> {
     // TODO: implement initState
     super.initState();
    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
-     _con.init(context);
+     _con.init(context, refresh);
    });
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _con.key,
       appBar: AppBar(
         leading: _menuDrawer(),
       ),
@@ -43,8 +45,9 @@ class _ClienteProductsListPageState extends State<ClienteProductsListPage> {
 
   Widget _menuDrawer(){
     return GestureDetector(
-      onTap: (){},
+      onTap: _con.openDrawer,
       child: Container(
+        margin: EdgeInsets.only(left: 20.0),
         alignment: Alignment.centerLeft,
         child: Image.asset('assets/img/menu.png', width: 20, height: 20,),
       ),
@@ -55,24 +58,95 @@ class _ClienteProductsListPageState extends State<ClienteProductsListPage> {
   Widget _drawer(){
     return Drawer(
       child: ListView(
+        padding: EdgeInsets.zero,
         children: [
-            DrawerHeader(child:
-            Column(
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: MyColors.primaryColor
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'Nombre de usuario',
+
+                  '${_con.user?.name ?? ''} ${_con.user?.lastname ?? ''}',
                   style: TextStyle(
                     fontSize: 18.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold
                   ),
                   maxLines: 1,
+                ),
+                Text(
+                  _con.user?.email ?? '',
+                  style: TextStyle(
+                      fontSize: 13.0,
+                      color: Colors.grey[300],
+                      fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic
+
+                  ),
+                  maxLines: 1,
+                ),
+                Text(
+                  _con.user?.phone ?? '',
+                  style: TextStyle(
+                      fontSize: 13.0,
+                      color: Colors.grey[300],
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic
+
+                  ),
+                  maxLines: 1,
+                ),
+                Container(
+                  height: 60.0,
+                  margin: EdgeInsets.only(top: 5.0),
+                  child: FadeInImage(
+                    image: _con.user?.image != null
+                        ? NetworkImage(_con.user!.image!) as ImageProvider<Object>
+                        : AssetImage('assets/img/no-image.png') as ImageProvider<Object>,
+
+                    fit: BoxFit.contain,
+                    fadeInDuration: Duration(milliseconds: 50),
+                    placeholder: AssetImage('assets/img/no-image.png'),
+                  ),
                 )
               ],
-            )
-            )
+            ),
+            ),
+          ListTile(
+            title: Text('Editar Perfil'),
+            trailing: Icon(Icons.edit),
+          ),
+          ListTile(
+            title: Text('Mis pedidos'),
+            trailing: Icon(Icons.shopping_cart),
+
+          ),
+          _con.user != null ?
+           _con.user!.roles!.length > 1 ?
+          ListTile(
+            onTap: _con.goToRoles,
+            title: Text('Seleccionar rol'),
+            trailing: Icon(Icons.person),
+
+          ):Container():Container(),
+
+          ListTile(
+            onTap: _con.logout,
+            title: Text('Cerrar sesión'),
+            trailing: Icon(Icons.power_settings_new),
+
+          ),
         ],
       ),
     );
+  }
+
+  void refresh(){
+    setState(() {
+
+    });
   }
 }
--- lib/pages/delivery/orders/list/delivery_orders_list_page.dart
+++ lib/pages/delivery/orders/list/delivery_orders_list_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:shoes/pages/delivery/orders/list/delivery_orders_list_controller.dart';
+
+import '../../../../src/utils/my_colors.dart';
 
 class DeliveryOrdersListPage extends StatefulWidget {
   const DeliveryOrdersListPage({super.key});
@@ -8,14 +12,137 @@ class DeliveryOrdersListPage extends StatefulWidget {
 }
 
 class _DeliveryOrdersListPageState extends State<DeliveryOrdersListPage> {
+
+  DeliveryOrdersListController _con = new DeliveryOrdersListController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+      _con.init(context, refresh);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key:_con.key,
+        drawer: _drawer(),
+        appBar: AppBar(
+          leading: _menuDrawer(),
+        ),
         body: Center(
           child: Text(
             'DELIVERY ORDERS LIST'
           ),
         ),
     );
+  }
+
+
+  Widget _menuDrawer(){
+    return GestureDetector(
+      onTap: _con.openDrawer,
+      child: Container(
+        margin: EdgeInsets.only(left: 20.0),
+        alignment: Alignment.centerLeft,
+        child: Image.asset('assets/img/menu.png', width: 20, height: 20,),
+      ),
+
+    );
+  }
+
+  Widget _drawer(){
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+                color: MyColors.primaryColor
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+
+                  '${_con.user?.name ?? ''} ${_con.user?.lastname ?? ''}',
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold
+                  ),
+                  maxLines: 1,
+                ),
+                Text(
+                  _con.user?.email ?? '',
+                  style: TextStyle(
+                      fontSize: 13.0,
+                      color: Colors.grey[300],
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic
+
+                  ),
+                  maxLines: 1,
+                ),
+                Text(
+                  _con.user?.phone ?? '',
+                  style: TextStyle(
+                      fontSize: 13.0,
+                      color: Colors.grey[300],
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic
+
+                  ),
+                  maxLines: 1,
+                ),
+                Container(
+                  height: 60.0,
+                  margin: EdgeInsets.only(top: 5.0),
+                  child: FadeInImage(
+                    image: _con.user?.image != null
+                        ? NetworkImage(_con.user!.image!) as ImageProvider<Object>
+                        : AssetImage('assets/img/no-image.png') as ImageProvider<Object>,
+
+                    fit: BoxFit.contain,
+                    fadeInDuration: Duration(milliseconds: 50),
+                    placeholder: AssetImage('assets/img/no-image.png'),
+                  ),
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            title: Text('Editar Perfil'),
+            trailing: Icon(Icons.edit),
+          ),
+          ListTile(
+            title: Text('Mis pedidos'),
+            trailing: Icon(Icons.shopping_cart),
+
+          ),
+          _con.user != null ?
+          _con.user!.roles!.length > 1 ?
+          ListTile(
+            onTap: _con.goToRoles,
+            title: Text('Seleccionar rol'),
+            trailing: Icon(Icons.person),
+
+          ):Container():Container(),
+          ListTile(
+            onTap: _con.logout,
+            title: Text('Cerrar sesión'),
+            trailing: Icon(Icons.power_settings_new),
+
+          ),
+        ],
+      ),
+    );
+  }
+
+  void refresh(){
+    setState(() {
+
+    });
   }
 }
