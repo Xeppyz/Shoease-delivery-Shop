7906ddab862f786ee85fb509daebadbe76211982
Xeppyz <alexander.xampos10@gmail.com>
2024-10-24 07:35:35 -0600

    feat(create):product

--- lib/pages/business/products/create/business_products_create_controller.dart
+++ lib/pages/business/products/create/business_products_create_controller.dart
@@ -1,9 +1,14 @@
+import 'dart:ffi';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shoes/src/models/category.dart';
+import 'package:shoes/src/models/product.dart';
 import 'package:shoes/src/models/response_api.dart';
 import 'package:shoes/src/provider/categories_provider.dart';
 import 'package:shoes/src/utils/my_snackbar.dart';
@@ -15,6 +20,10 @@ class BusinessProductsCreateController{
 
   BuildContext? context;
   Function? refresh;
+  PickedFile? pickedFile;
+  File? imageFile1;
+  File? imageFile2;
+  File? imageFile3;
 
   TextEditingController nameController = new TextEditingController();
   TextEditingController descriptionController = new TextEditingController();
@@ -41,12 +50,41 @@ class BusinessProductsCreateController{
   void createProduct() async{
     String name = nameController.text;
     String description = descriptionController.text;
-    String price = priceController.text;
+    // Quitar símbolos y separadores para convertir correctamente a double.
+    String cleanText = priceController.text
+        .replaceAll(RegExp(r'[^0-9.]'), ''); // Mantén solo números y punto decimal
 
-    if(name.isEmpty || description.isEmpty){
+    double? price = double.tryParse(cleanText);
+
+    if (price == null) {
+      print("Error: Precio no válido");
+      return;
+    }
+
+
+    if(name.isEmpty || description.isEmpty || price == 0){
       MySnackBar.show(context!, 'Debe de ingresar todos los campos');
       return;
     }
+
+    if(imageFile1 == null || imageFile2 == null || imageFile3 == null){
+      MySnackBar.show(context!, 'Selecciona las tres imagenes');
+      return;
+    }
+    if(idCategory == null){
+      MySnackBar.show(context!, 'Selecciona una categoria');
+      return;
+    }
+
+    Product product = new Product(
+        name: name,
+        description: description,
+        price: price,
+        idCategory: int.parse(idCategory!),
+
+    );
+
+    print("formulario Producto ${product.toJson()}");
 
     print("name ${name}");
     print("description ${description}");
@@ -54,4 +92,52 @@ class BusinessProductsCreateController{
 
 
     }
+
+  Future selectImage(ImageSource imageSource, int numberFile) async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? pickedFile = await _picker.pickImage(source: imageSource);
+    if (pickedFile != null) {
+      if(numberFile == 1){
+        imageFile1 = File(pickedFile.path);
+      }
+      else if(numberFile == 2){
+        imageFile2 = File(pickedFile.path);
+      }
+      else if(numberFile == 3){
+        imageFile3 = File(pickedFile.path);
+      }
+    }
+    Navigator.pop(context!);
+    refresh!();
+  }
+
+
+  void showAlertDialog(int numberFile){
+    Widget _galleryButton = ElevatedButton(
+        onPressed: (){
+          selectImage(ImageSource.gallery, numberFile);
+        },
+        child: Text('GALERIA')
+    );
+    Widget _cameraButton = ElevatedButton(
+        onPressed: (){
+          selectImage(ImageSource.camera, numberFile);
+        },
+        child: Text('CAMARA')
+    );
+    AlertDialog alertDialog = AlertDialog(
+      title: Text('Selecciona tu imagen'),
+      actions: [
+        _galleryButton,
+        _cameraButton
+      ],
+    );
+
+    showDialog(
+        context: context!,
+        builder: (BuildContext? context){
+          return alertDialog;
+        }
+    );
+  }
   }
---
+++ lib/src/models/product.dart
@@ -0,0 +1,57 @@
+// To parse this JSON data, do
+//
+//     final product = productFromJson(jsonString);
+
+import 'dart:convert';
+
+Product productFromJson(String str) => Product.fromJson(json.decode(str));
+
+String productToJson(Product data) => json.encode(data.toJson());
+
+class Product {
+  String? id;
+  String name;
+  String description;
+  String? image1;
+  String? image2;
+  String? image3;
+  double price;
+  int idCategory;
+  int? quantity;
+
+  Product({
+     this.id,
+    required this.name,
+    required this.description,
+     this.image1,
+     this.image2,
+     this.image3,
+    required this.price,
+    required this.idCategory,
+     this.quantity,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+    id: json["id"],
+    name: json["name"],
+    description: json["description"],
+    image1: json["image1"],
+    image2: json["image2"],
+    image3: json["image3"],
+    price: json["price"]?.toDouble(),
+    idCategory: json["id_category"]?.toDouble(),
+    quantity: json["quantity"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "description": description,
+    "image1": image1,
+    "image2": image2,
+    "image3": image3,
+    "price": price,
+    "id_category": idCategory,
+    "quantity": quantity,
+  };
+}
